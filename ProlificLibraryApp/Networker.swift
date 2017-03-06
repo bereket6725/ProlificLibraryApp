af21503b7b93cb657cc1b083a181cc8ec86d7682
Bereket <bereket6725@gmail.com>
2017-03-06 16:15:41 -0500

    starting building out the networker again. Also make the book model

---
+++ ProlificLibraryApp/Networker.swift
@@ -0,0 +1,9 @@
+//
+//  Networker.swift
+//  ProlificLibraryApp
+//
+//  Created by Bereket Ghebremedhin on 3/6/17.
+//  Copyright © 2017 Bereket Ghebremedhin. All rights reserved.
+//
+
+import Foundation
---
+++ ProlificLibraryApp/ViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ViewModel.swift
+//  ProlificLibraryApp
+//
+//  Created by Bereket Ghebremedhin on 3/6/17.
+//  Copyright © 2017 Bereket Ghebremedhin. All rights reserved.
+//
+
+import Foundation
