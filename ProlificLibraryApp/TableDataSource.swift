5c03b570fdaacaad254a427d6b4139fb013ce49c
Bereket <bereket6725@gmail.com>
2017-03-06 17:28:54 -0500

    Set up TableViewController and its Data Source

---
+++ ProlificLibraryApp/TableDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  TableDataSource.swift
+//  ProlificLibraryApp
+//
+//  Created by Bereket Ghebremedhin on 3/6/17.
+//  Copyright © 2017 Bereket Ghebremedhin. All rights reserved.
+//
+
+import Foundation
